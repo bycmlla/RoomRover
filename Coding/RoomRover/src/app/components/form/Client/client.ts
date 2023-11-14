2e328eebca64a45f967eb567a3d1245af26268a8
Camila <by.cmlla0107@gmail.com>
2023-11-14 19:42:22 -0300

    saving for dont lost this

---
+++ Coding/RoomRover/src/app/components/form/Client/client.ts
@@ -0,0 +1,9 @@
+export class Client {
+  name: string = '';
+  email: string = '';
+  phone: string = '';
+  birthdate: Date = new Date();
+  nationality: string = '';
+  gender: string = '';
+  password: string = '';
+}
--- Coding/RoomRover/src/app/components/form/form.component.ts
+++ Coding/RoomRover/src/app/components/form/form.component.ts
@@ -1,11 +1,36 @@
-import { Component, NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { Client } from 'src/app/components/form/Client/client'; 
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent {
 
+export class FormComponent implements OnInit {
+  
+  formClient!: FormGroup;
+  constructor() {}
+
+  ngOnInit(): void {
+      this.createForm(new Client())
+  }
+  
+  createForm(client: Client){
+    this.formClient = new FormGroup({
+      nome: new FormControl(client.name),
+      email: new FormControl(client.email),
+      phone: new FormControl(client.phone),
+      nascimento: new FormControl(client.birthdate),
+      nacionalidade: new FormControl(client.nationality),
+      genero: new FormControl(client.gender),
+      senha: new FormControl(client.password)
+    })
+  }
+  onSubmit(){
+    console.log(this.formClient.value)
+  }
+  
+  
 }
