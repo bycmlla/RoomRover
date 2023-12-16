bd1ad65d3cbb7a67bdd41b9c81e607ab5769c921
Camila <by.cmlla0107@gmail.com>
2023-12-16 09:58:42 -0300

    some changes

--- Coding/RoomRover/server/index.js
+++ Coding/RoomRover/server/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const server = express();
 const port = 3000;
 const router = require("./api/mainRouter");
+const cors = require('cors');
+
+server.use(cors());
 
 router(server);
 
