8596ab8b59afe7eb099f593d17a385d4c3df6993
Camila <by.cmlla0107@gmail.com>
2024-01-08 19:29:07 -0300

    to change branche

--- Coding/RoomRover/server/index.js
+++ Coding/RoomRover/server/index.js
@@ -3,13 +3,14 @@ const server = express();
 const port = 3000;
 const router = require("./api/mainRouter");
 const cors = require('cors');
+const bodyParser = require("body-parser");
 
 server.use(cors());
-
+server.use(bodyParser.json());
 router(server);
 
 server.listen(port, (error) => {
-  if (error) {
+  if (error) {disc
     console.log("Error");
     return;
   }
