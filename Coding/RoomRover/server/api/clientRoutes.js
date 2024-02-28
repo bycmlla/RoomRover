ff028bbadfd6dafffa3d3e8f9121f7cf42d347df
Camila <by.cmlla0107@gmail.com>
2024-02-28 19:13:54 -0300

    update

--- Coding/RoomRover/server/api/clientRoutes.js
+++ Coding/RoomRover/server/api/clientRoutes.js
@@ -25,36 +25,39 @@ router.post("/form/add", (req, res) => {
           .send({ status: false, message: "Erro ao criar endereço" });
       } else {
         console.log("Endereço criado com sucesso.");
+
+        let clientDetails = {
+          name: req.body.nome,
+          email: req.body.email,
+          phone: req.body.phone,
+          birthdate: req.body.nascimento,
+          nationality: req.body.nacionalidade,
+          gender: req.body.genero,
+          password: req.body.senha,
+          idaddressfk: addressResponse.insertId,
+        };
+
+        let sql = "INSERT INTO roomrover.client SET ?";
+
+        connection.query(sql, clientDetails, (error, response) => {
+          if (error) {
+            console.error("Erro ao criar:", error);
+            res
+              .status(500)
+              .send({ status: false, message: "Erro ao criar" });
+          } else {
+            console.log("Deu certo");
+            res.setHeader("Content-Type", "application/json");
+            res
+              .status(200)
+              .send({ status: true, message: "Registro criado com sucesso" });
+          }
+        });
       }
-
-      let clientDetails = {
-        name: req.body.nome,
-        email: req.body.email,
-        phone: req.body.phone,
-        birthdate: req.body.nascimento,
-        nationality: req.body.nacionalidade,
-        gender: req.body.genero,
-        password: req.body.senha,
-        idaddressfk: addressResponse.insertId,  
-      };
-
-      let sql = "INSERT INTO roomrover.client SET ?";
-
-      connection.query(sql, clientDetails, (error, response) => {
-        if (error) {
-          console.error("Erro ao criar:", error);
-          res.status(500).send({ status: false, message: "Erro ao criar" });
-        } else {
-          console.log("Deu certo");
-          res.setHeader("Content-Type", "application/json");
-          res
-            .status(200)
-            .send({ status: true, message: "Registro criado com sucesso" });
-        }
-      });
     }
   );
 });
+
 
 router.get("/form/read", (req, res) => {
   const sql = "SELECT * FROM roomrover.client";
