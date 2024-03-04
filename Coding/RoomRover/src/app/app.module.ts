e04b215b2cc9defd0b9922dcbdea212d645429c6
Camila <by.cmlla0107@gmail.com>
2024-03-04 19:46:05 -0300

    create front-end

--- Coding/RoomRover/src/app/app.module.ts
+++ Coding/RoomRover/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
+import { NavbarComponent } from './components/navbar-simple/navbar.component';
 import {HttpClientModule} from '@angular/common/http';
 import { ClientService } from './services/api/apiservice.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,6 +11,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NavbarMainComponent } from './components/navbar-main/navbar-main.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { NavbarMainComponent } from './components/navbar-main/navbar-main.compon
     HomeComponent,
     SignupComponent,
     LoginComponent,
-    NavbarMainComponent
+    NavbarMainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
