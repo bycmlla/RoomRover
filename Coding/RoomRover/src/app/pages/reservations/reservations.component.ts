c33500d90f1c9e57c407c7d425fcf2a11139adce
Camila <by.cmlla0107@gmail.com>
2024-03-11 20:33:31 -0300

    updates

--- Coding/RoomRover/src/app/pages/reservations/reservations.component.ts
+++ Coding/RoomRover/src/app/pages/reservations/reservations.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-reservations',
   templateUrl: './reservations.component.html',
   styleUrls: ['./reservations.component.scss']
 })
 export class ReservationsComponent {
+  constructor(private router: Router) {}
 
+  navigateToDetails() {
+    this.router.navigate(['/reservations-details']);
+  }
 }
